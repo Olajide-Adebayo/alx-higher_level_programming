524b35166a92905f053df208e2f048d482f4a818
Olajide-Adebayo <jideinspires@gmail.com>
2022-01-13 16:19:32 +0000

    <lists.h>

---
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -0,0 +1,5 @@
+def print_last_digit(number):
+			    def add(a, b):
+					 def pow(a, b):
+						      listint_t *insert_node(listint_t **head, int number);
+						      def remove_char_at(str, n):
